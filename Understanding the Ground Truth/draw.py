19c87852a70caf2bef1231f4b2fe038bc548230c
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2018-12-19 20:54:07 +0300

    Structuring stuff

---
+++ Understanding the Ground Truth/draw.py
@@ -0,0 +1,9 @@
+import cv2
+
+frame=cv2.imread("00130.jpeg")
+
+cv2.rectangle(frame, (601,431), (626,456), (255, 255, 00), 2)
+cv2.imshow("frame",frame)
+cv2.imwrite("GT-Shown.jpeg",frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
