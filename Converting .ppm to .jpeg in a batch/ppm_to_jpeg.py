38d387b587b604d4d32fe1c21fe60f7de849ccb1
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2018-12-19 21:07:37 +0300

    Update ppm_to_jpeg.py

--- Converting .ppm to .jpeg in a batch/ppm_to_jpeg.py
+++ Converting .ppm to .jpeg in a batch/ppm_to_jpeg.py
@@ -3,10 +3,13 @@ from PIL import Image
 import os
 import glob
 
-file_open = lambda x,y: glob.glob(os.path.join(x,y))
-image_dir1 = "C:/Users/msais/Desktop/GTechSummer/images"
-images = file_open(image_dir1,"*.ppm")
 
+file_open = lambda x,y: glob.glob(os.path.join(x,y))
+#image directory path
+image_dir1 = "C:/Users/msais/Desktop/GTechSummer/images"
+# getting .ppm files in the image
+images = file_open(image_dir1,"*.ppm")
+#iterate thru all the images
 for image in images:
     im = Image.open("{}".format(image))
     im.save("{}.jpeg".format(image[-9:-4]))
